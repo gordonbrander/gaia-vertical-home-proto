be8873567d798442f88544ec108fc7ad60f36fd0
Gordon Brander <marlfoskr@gmail.com>
2014-03-17 17:03:20 -0600

    Working proof of grid tweening function

--- index.js
+++ index.js
@@ -17,160 +17,99 @@
 
 // [{ weight: 200.2, value: 'foo' }]
 
-// Access a value at index `i`.
-function at(indexed, i) {
-  // If `indexed` is a value, not an indexed object, return value itself.
-  if (indexed == null || indexed.length == null) return indexed;
-  // If i is within range, treat this as standard property access.
-  else if (i < indexed.length) return indexed[i];
-  // If `i` is greater than length, use the "repeat last item" rule.
-  // This makes it easy to zip 2 items of different length, for example.
-  else return indexed[len - 1];
-}
-
-// Fold a value by zipping `a` and `b`, where:
-// `a` is an indexed object.  
-// `b` is an indexed object or any other value.
-function cofold(a, b, step, folded) {
-  // We use `a` as the driver for iteration.
-  for (var i = 0, length = a.length; i < length; i++)
-    // Note that since `b` is accessed via `at()`, it can be a plain value.
-    folded = step(folded, a[i], at(b, i), i);
-  return folded;
-}
-
-// Assign a new `item` to vector `v` at index `i`.
-// Mutates and returns vector `v`.
-function assignV(v, i, item) {
-  // Assign item to property.
-  v[i] = item;
-  // If length is not properly handled by this datatype, update it manually.
-  if (!v.length || i >= v.length) v.length = i + 1;
-  return v;
-}
-
-function assignSub(into, n0, n1, i) {
-  return assignV(into, i, n0 - n1);
-}
-
-// Subtract v1 from v0, returning mutated vector `into`.
-function subV(into, v0, v1) {
-  return cofold(v0, v1, assignSub, into);
-}
-
-function assignMult(into, n0, n1, i) {
-  return assignV(into, i, n0 * n1);
-}
-
-// Multiply `v0` by `v1`, returning mutated vector `into`.
-function multV(into, v0, v1) {
-  return cofold(v0, v1, assignMult, into);
-}
-
-function tweenV(into, from, to, factor) {
-  return multV(into, subV(into, to, from), factor);
-}
-
-// Find the column this item belongs to.
-function calcGridCol(i, cols) {
-  return (i % cols);
-}
-
-// Find the row this item belongs to.
-function calcGridRow(i, cols) {
-  return Math.floor(i / cols);
-}
-
-function calGridLeft(i, cols, w) {
-  return calcGridCol(i, cols) * w;
-}
-
-function calcGridTop(i, cols, h) {
-  return calcGridRow(i, cols) * h;
-}
-
-function makeUnitStats(i, w, h, grid, cols) {
-  var colW = gridW / cols;
-  return [
-    w,
-    h,
-    calcGridLeft(i, cols, w) + ((colW - w) / 2),
-    calcGridTop(i, cols, h)
-  ];
-}
-
 function translate3d(element, x, y, z) {
   element.style.transform = 'translate3d(' + x + 'px, ' + y + 'px, ' + z + 'px)';
   return element;
 }
 
-function fold4(indexed, step, initial, a, b, c, d) {
-  var folded = initial;
-  for (var i = 0; i < indexed.length; i++)
-    folded = step(indexed[i], folded, a, b, c, d);
-  return folded;
+function each(indexed, step, a, b, c, d) {
+  for (var i = 0; i < indexed.length; i++) step(indexed[i], i, a, b, c, d);
+  return indexed;
 }
 
-function foldIntoGrid_(element, i, w, h, gridW, cols) {
-  // Find the column slot width.
-  var colW = (gridW / cols);
-
-  // Left offset = position over to column, then add additional "padding"
-  // to center content in slot.
-  var offsetLeft = (calcGridCol(i, cols) * colW) + ((colW - w) / 2);
-  var offsetTop = (calcGridRow(i, cols) * h);
-
-  element.style.width = w + 'px';
-
-  translate3d(element, offsetLeft, offsetTop, 0)
-
-  return i + 1;
+function tween(from, to, factor) {
+  return from + ((to - from) * factor);
 }
 
-function layoutEachInGrid(elements, w, h, gridW, cols) {
-  fold4(elements, foldIntoGrid_, 0, w, h, gridW, cols);
-  return elements;
+function calcGridLeft(i, cols, w) {
+  return (i % cols) * w;
 }
 
-// Significant dimensions:
-// 
-// Column width
-// Unit width
-// Unit height
-// Number of columns
-// Grid width
-// 
-// The most useful driving dimensions are:
-// 
-// Grid width
-// Number of columns
-// Unit height
+function calcGridTop(i, cols, h) {
+  return Math.floor(i / cols) * h;
+}
 
-function layoutGrid(gridEl, gridW, unitH, cols) {
-  // Any child of `gridEl` is treated as the element to be layed out, for now.
+function layoutEl(el, w, h, l, t) {
+  el.style.width = w + 'px';
+  translate3d(el, l, t, 0);
+  return el;
+}
+
+function tweenGridLayout(gridEl, fromGridW, fromUnitH, fromCols, toGridW, toUnitH, toCols, factor) {
+  // Units are assumed to be direct children of grid for now.
   var unitEls = gridEl.children;
 
+  // Calculate unit width from desired grid width.
+  var fromUnitW = (fromGridW / fromCols);
+  var toUnitW = (toGridW / toCols);
+
+  var unitW = tween(fromUnitW, toUnitW, factor);
+  var unitH = tween(fromUnitH, toUnitH, factor);
+
   // Calc desired static height of grid.
-  var gridH = calcGridTop(unitEls.length - 1, cols, unitH) + unitH;
+  var gridH = tween(
+    calcGridTop(unitEls.length - 1, fromCols, fromUnitH) + fromUnitH,
+    calcGridTop(unitEls.length - 1, toCols, toUnitH) + toUnitH,
+    factor
+  );
 
-  // Set dimensions of `gridEl`.
+  var gridW = tween(
+    fromGridW,
+    toGridW,
+    factor
+  );
+
+  // Set static dimensions for `gridEl`.
   gridEl.style.width = gridW + 'px';
-  gridEl.style.height = gridH + 'px';
+  gridEl.style.height = gridH + 'px';  
 
-  // Layout children in grid.
-  layoutEachInGrid(unitEls, (gridW / cols), unitH, gridW, cols);
+  each(unitEls, function(el, i) {
+    var elL = tween(
+      calcGridLeft(i, fromCols, fromUnitW),
+      calcGridLeft(i, toCols, toUnitW),
+      factor
+    );
+
+    var elT = tween(
+      calcGridTop(i, fromCols, fromUnitH),
+      calcGridTop(i, toCols, toUnitH),
+      factor
+    );
+
+    layoutEl(el, unitW, unitH, elL, elT);
+  });
+
+  return unitEls;
 }
 
-function setupDemo(gridEl, unitEls) {
+function gridLayoutTweener(fromGridW, fromUnitH, fromCols, toGridW, toUnitH, toCols) {
+  return (function tweenGridLayout_(gridEl, factor) {
+    return tweenGridLayout(gridEl, fromGridW, fromUnitH, fromCols, toGridW, toUnitH, toCols, factor);
+  });
+}
+
+function setupDemo(gridEl) {
   var isThreeUp = true;
 
-  layoutGrid(gridEl, 300, 120, 3);
+  var tweenGrid = gridLayoutTweener(300, 120, 3, 300, 110, 4);
+
+  tweenGrid(gridEl, 0);
 
   gridEl.addEventListener('click', function (event) {
     // Toggle state
     isThreeUp = !isThreeUp;
 
-    if (isThreeUp) layoutGrid(gridEl, 300, 120, 3);
-    else layoutGrid(gridEl, 300, 110, 4);
+    if (isThreeUp) tweenGrid(gridEl, 0);
+    else tweenGrid(gridEl, 1);
   });
 }
